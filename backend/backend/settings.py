f25e5e0fd7fd9112e08da437777144f156d8c7ff
Josiah Chinedu <chinedujosiahjkl@gmail.com>
2025-11-12 00:07:38 +0100

    Add gunicorn to requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s$0q8r-pkwk6024-*g%gl@=59^*d5pqvty)&=_x4y7c76rgb+u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
